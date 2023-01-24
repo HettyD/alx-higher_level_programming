6f259e21c1909efc5409cc8d87135e6322caad3b
Henrietta <113603861+HettyD@users.noreply.github.com>
2023-01-24 22:32:49 +0100

    Update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,60 +1,80 @@
 #!/usr/bin/python3
-"""Square class to represent a square"""
+
+"""Define classes for a singly-linked list."""
 
 
-class Square():
-    """square class with it's size and proper validation"""
+class Node:
+    """Represent a node in a singly-linked list."""
 
-    def __init__(self, size=0, position=(0, 0)):
-        """"Initialize data"""
-        self.size = size
-        self.position = position
+    def __init__(self, data, next_node=None):
+        """Initialize a new Node.
+
+        Args:
+            data (int): The data of the new Node.
+            next_node (Node): The next node of the new Node.
+        """
+        self.data = data
+        self.next_node = next_node
 
     @property
-    def size(self):
-        """"get size"""
-        return self.__size
+    def data(self):
+        """Get/set the data of the Node."""
+        return (self.__data)
+
+    @data.setter
+    def data(self, value):
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        self.__data = value
 
     @property
-    def position(self):
-        """"get position"""
-        return self.__position
+    def next_node(self):
+        """Get/set the next_node of the Node."""
+        return (self.__next_node)
 
-    @size.setter
-    def size(self, value):
-        """"set size"""
-        if (type(value) is not int):
-            raise TypeError("size must be an integer")
-        elif (value < 0):
-            raise ValueError("size must be >= 0")
+    @next_node.setter
+    def next_node(self, value):
+        if not isinstance(value, Node) and value is not None:
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
+
+class SinglyLinkedList:
+    """Represent a singly-linked list."""
+
+    def __init__(self):
+        """Initalize a new SinglyLinkedList."""
+        self.__head = None
+
+    def sorted_insert(self, value):
+        """Insert a new Node to the SinglyLinkedList.
+
+        The node is inserted into the list at the correct
+        ordered numerical position.
+
+        Args:
+            value (Node): The new Node to insert.
+        """
+        new = Node(value)
+        if self.__head is None:
+            new.next_node = None
+            self.__head = new
+        elif self.__head.data > value:
+            new.next_node = self.__head
+            self.__head = new
         else:
-            self.__size = value
+            tmp = self.__head
+            while (tmp.next_node is not None and
+                    tmp.next_node.data < value):
+                tmp = tmp.next_node
+            new.next_node = tmp.next_node
+            tmp.next_node = new
 
-    @position.setter
-    def position(self, value):
-        """"set position"""
-        if (type(value) is not tuple):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif (len(value) != 2):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif (type(value[0]) is not int) or (type(value[1]) is not int):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif (value[0] < 0) or (value[1] < 0):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        else:
-            self.__position = value
-
-    def area(self):
-        """"get area of the square"""
-        return self.size ** 2
-
-    def my_print(self):
-        """print the square"""
-        if self.size == 0:
-            print()
-        else:
-            for i in range(self.position[1]):
-                print("")
-            for i in range(self.size):
-                print(" " * self.position[0], end="")
-                print("#" * self.size)
+    def __str__(self):
+        """Define the print() representation of a SinglyLinkedList."""
+        values = []
+        tmp = self.__head
+        while tmp is not None:
+            values.append(str(tmp.data))
+            tmp = tmp.next_node
+        return ('\n'.join(values))
