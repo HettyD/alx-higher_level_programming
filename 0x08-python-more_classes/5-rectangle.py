70e00cef426a126847053e369b9fab77afed2290
HettyD <hettydagogz93@gmail.com>
2023-01-30 12:41:27 -0800

    tasks

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""
+Defines a class Rectangle
+"""
+
+
+class Rectangle:
+    """Representation of a rectangle"""
+    def _init_(self, width=0, height=0):
+        """Initializes the rectangle"""
+        self.width = width
+        self.height = height
+
+    def _del_(self):
+        """prints a string when an instance has been deleted"""
+        print("Bye rectangle...")
+
+    @property
+    def width(self):
+        """getter for the private instance attribute width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setter for the private instance attribute width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """getter for the private instance attribute height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setter for the private instance attribute height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """returns the area of the rectangle"""
+        return self._width * self._height
+
+    def perimeter(self):
+        """returns the perimeter of the rectangle"""
+        if self._width == 0 or self._height == 0:
+            return 0
+        return (self._width * 2) + (self._height * 2)
+
+    def _str_(self):
+        """returns printable string representation of the rectangle"""
+        string = ""
+        if self._width != 0 and self._height != 0:
+            string += "\n".join("#" * self.__width
+                                for j in range(self.__height))
+        return string
+
+    def _repr_(self):
+        """returns a string representation of the rectangle for reproduction"""
+        return "Rectangle({:d}, {:d})".format(self._width, self._height)
